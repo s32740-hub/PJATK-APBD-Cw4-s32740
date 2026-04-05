3455f137ca4e43b8a3c7af1418e0aa7171afd7f3
s32740 <s32740@pjwstk.edu.pl>
2026-04-05 15:43:03 +0200

    feat: refactoring tax calculations

---
+++ LegacyRenewalApp/TaxCalculators.cs
@@ -0,0 +1,37 @@
+﻿namespace LegacyRenewalApp;
+
+public class TaxCalculatorPoland : ITaxCalculator
+{
+    public bool IsMatch(string country) => country == "Poland";
+    public decimal Calculate()
+    {
+        return 0.23m;
+    }
+}
+
+public class TaxCalculatorGermany : ITaxCalculator
+{
+    public bool IsMatch(string country) => country == "Germany";
+    public decimal Calculate()
+    {
+        return 0.19m;
+    }
+}
+
+public class TaxCalculatorCzech : ITaxCalculator
+{
+    public bool IsMatch(string country) => country == "Czech Republic";
+    public decimal Calculate()
+    {
+        return 0.21m;
+    }
+}
+
+public class TaxCalculatorNorway : ITaxCalculator
+{
+    public bool IsMatch(string country) => country == "Norway";
+    public decimal Calculate()
+    {
+        return 0.25m;
+    }
+}
